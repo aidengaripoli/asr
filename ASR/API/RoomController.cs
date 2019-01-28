fab36d68ad955ed39e1dd4088ad5ae2c292dfb5b
hayden warmington <haydenwarmington@gmail.com>
2019-01-28 16:38:32 +1100

    scaffold web api

---
+++ ASR/API/RoomController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ASR.Models;
+
+namespace ASR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly ASRContext _context;
+
+        public RoomController(ASRContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Room
+        [HttpGet]
+        public IEnumerable<Room> Get()
+        {
+            return _context.Room.ToList();
+        }
+
+        // GET: api/Room/<room-id>
+        [HttpGet("{id}", Name = "GetRoom")]
+        public Room Get(string id)
+        {
+            var room = _context.Room.FirstOrDefault( r => r.RoomID == id);
+            return room;
+        }
+
+        // POST: api/Room
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Room/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ASR/API/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ASR.Data;
+using ASR.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Net.Http;
+using System.Net;
+
+namespace ASR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly ASRContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(ASRContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/User/<role>
+        [HttpGet("{role}", Name = "GetUsers")]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> Get(string role)
+        {
+            if (role.ToLower() == Constants.StaffRole.ToLower())
+            {
+                var staffs = await _userManager.GetUsersInRoleAsync(Constants.StaffRole);
+                return staffs.ToList();
+            }
+            else if (role.ToLower() == Constants.StudentRole.ToLower())
+            {
+                var students = await _userManager.GetUsersInRoleAsync(Constants.StudentRole);
+                return students.ToList();
+            }
+            return BadRequest();
+
+        }
+    }
+}
